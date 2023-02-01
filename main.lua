d129835aa793a78af290235c3f1467445eeaad50
Hamdy Elzonqali <hamdy.elzanqali@gmail.com>
2023-02-01 15:45:20 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,46 +1,47 @@
+--[[
+    I recommend turning on "Minify JSON" and "Save levels to separate files" in project settings 
+    to save space and increase loading speed.
+
+    You can add your tileset images anywhere relative to main.lua.
+    You have to save the LDtk project for it to take effect.
+
+    As noted in https://love2d.org/wiki/Quad, you may notice bleeding when moving and scaling. 
+    To fix this you have to add a border of the same color to every tile in the tileset then set 
+    a spacing of 2px and a padding of 1px to the tileset in LDtk.
+    If that sounds like too much work, I've already written a script that does it for you in Aseprite:
+    https://github.com/HamdyElzonqali/BleedingFixer-Aseprite-Love2d 
+]]
+
 --loading ldtk library
 local ldtk = require 'ldtk'
 
---[[
-    I recommend checking "Minify JSON", "Save levels to separate files"
-    and "Advanced --> discard pre-CSV..." in project settings to save space
-    and increase loading speed.
-
-    You can add your tilesets' images anywhere relative to main.lua
-
-    You have to save the LDtk project to take effect.
-    You don't have to relaunch the game to get edited levels if you use separate
-    files. But, You will need to relaunch or LDtk:load(file) if you add new levels or
-    you have a single file.
-
-    You may notice bleeding when moving and scaling. To fix this you have to add a border of the
-    same color to every tile in the tileset then set a spacing of 2px and a padding of 1px to the
-    tile in LDtk. Check LÖVE wiki to know more about bleeding and how to fix it.
-    https://love2d.org/wiki/Quad
-]]
-
-local objects = {} --all objects are here
+--objects table
+local objects = {}
 
 
+-------- ENTITIES --------
 --classes are used for the example
 local class = require 'classic'
 
 --object class 
 local object = class:extend()
 
-function object:new(e)
-    self.x, self.y = e.x, e.y
-    self.w, self.h = e.width, e.height
-    self.visible = e.visible
+function object:new(entitiy)
+    -- setting up the object using the entity data
+    self.x, self.y = entitiy.x, entitiy.y
+    self.w, self.h = entitiy.width, entitiy.height
+    self.visible = entitiy.visible
 end
 
 function object:draw()
     if self.visible then
-        --drawing a rectangle to represent the entity
+        --draw a rectangle to represent the entity
         love.graphics.rectangle('fill', self.x, self.y, self.w, self.h)
     end
 end
---
+-------------------------
+
+
 
 function love.load()
     --resizing the screen to 512px width and 512px height
@@ -51,57 +52,117 @@ function love.load()
     love.graphics.setLineStyle('rough')
     --
 
-    --loads a .ldtk file
+    --loading the .ldtk file
     ldtk:load('ldtk/game.ldtk')
-    ldtk:setFlipped(true) --flips the loop
-                          --you may not need this if you have your own custom loop
+    --ldtk:load('samples/AutoLayers_6_OptionalRules.ldtk')
 
     --[[
-        This is called when an entity is created
-        
-        entity = {x = (int), y = (int), width = (int), height = (int), visible = (bool)
-                    px = (int), py = (int), order = (int), props = (table)}
-        
-        px is pivot x and py is pivot y
-        props contains all custom fields defined in LDtk for that entity
-        remember that colors are HEX not RGB. 
-        You can use ldtk ldtk.getColorHex(color) to get an RGB table like {0.21, 0.57, 0.92}
+        This defines whether to filp the order or not. It's false by default.
+        It can be useful if the drawing order is flipped as in this situation.
     ]]
-    function ldtk.entity(entity)
+    ldtk:setFlipped(true)
+
+    --[[
+        This library depends heavily on callbacks. It works by overriding the default callbacks.
+    ]]
+
+    --[[
+        ldtk.onEntity is called when a new entity is created.
+        
+        entity = {
+            id          = (string), 
+            x           = (int), 
+            y           = (int), 
+            width       = (int), 
+            height      = (int), 
+            visible     = (bool)
+            px          = (int),    --pivot x
+            py          = (int),    --pivot y
+            order       = (int), 
+            props       = (table)   --custom fields defined in LDtk
+        }
+        
+        Remember that colors are saved in HEX format and not RGB. 
+        You can use ldtk ldtk.hex2rgb(color) to get an RGB table like {0.21, 0.57, 0.92}
+    ]]
+    function ldtk.onEntity(entity)
+        --[[
+            An example on how data could be used to create in-game objects.
+            Generally, you would define a custom field in LDtk to determine the object that the entity represents.
+            Ex. game_objects[entity.props.type]() --game_objects is a table that contains all the objects classes in the game.
+        ]]
         local newObject = object(entity) --creating new object based on the class we defined before
         table.insert(objects, newObject) --add the object to the table we use to draw
     end
 
     --[[
-        This is called when a new layer object is created
-        The given object has x, y, order, identifier, visible, color and a draw function
+        ldtk.onLayer is called when a new layer is created.    
+    
         layer:draw() --used to draw the layer
+
+        layer = {
+            id          = (string), 
+            x           = (int), 
+            y           = (int), 
+            visible     = (bool)
+            color       = (table),  --the color of the layer {r,g,b,a}. Usually used for opacity.
+            order       = (int),
+            draw        = (function) -- used to draw the layer
+        }
     ]]
-    function ldtk.layer(layer) 
+    function ldtk.onLayer(layer)
+        -- Here we treated the layer as an object and added it to the table we use to draw.
+        -- Generally, you would create a new object and use that object to draw the layer.
+
         table.insert(objects, layer) --adding layer to the table we use to draw 
     end
 
     --[[
-        This is called before we create the new level.
-        You may use it to remove old objects and change some settings like background color
-        level = {bgColor = (table), identifier = (string), worldX  = (int), worldY = (int), 
-                 width = (int), height = (int), props = (table)}
+        ldtk.onLevelLoaded is called after the level data is loaded but before it's created.
+
+        It's usually useful when you need to remove old objects and change some settings like background color
+
+        level = {
+            id          = (string), 
+            worldX      = (int), 
+            worldY      = (int), 
+            width       = (int), 
+            height      = (int), 
+            props       = (table), --custom fields defined in LDtk
+            backgroundColor = (table) --the background color of the level as defined in LDtk
+        }
         
         props table has the custom fields defined in LDtk
     ]]
-    function ldtk.onLevelLoad(level)
-        objects = {} --removing all objects so we can create our new room
-        love.graphics.setBackgroundColor(level.bgColor) --changing background color
+    function ldtk.onLevelLoaded(level)
+        --removing all objects so we have a blank level
+        objects = {}
+
+        --changing background color to the one defined in LDtk
+        love.graphics.setBackgroundColor(level.backgroundColor)
     end
 
     --[[
-        This is called after the new level is created. (after creating all layers and entities)
-        You may use it to change some settings for objects or to call a function.
-        level = {bgColor = (table), identifier = (string), worldX  = (int), worldY = (int), 
-                 width = (int), height = (int), props = (table)}
+        ldtk.onLevelCreated is called after the level is created.
+
+        It's usually useful when you need to call a function or manipulate the objects after they are created.
+
+        level = {
+            id          = (string), 
+            worldX      = (int), 
+            worldY      = (int), 
+            width       = (int), 
+            height      = (int), 
+            props       = (table), --custom fields defined in LDtk
+            backgroundColor = (table) --the background color of the level as defined in LDtk
+        }
     ]]
+
     function ldtk.onLevelCreated(level)
-        load(level.props.create)() --here we use a string defined in LDtk as a function
+        --Here we use a string defined in LDtk as a function
+        if level.props.create then 
+            load(level.props.create)() 
+        end
     end
 
     --Loading the first level.
@@ -121,12 +182,10 @@ function love.load()
         You can reload current level (if player loses for example)
         ldtk:reload()
     ]]
-    
 end
 
 
 -- keyboard keys switch statement for lua
--- this is much faster than if - elseif
 local keys = {
     right = function ()
         ldtk:next()
@@ -145,14 +204,16 @@ function love.keypressed(k)
 end
 
 
-local len
 function love.draw()
-   love.graphics.scale(2, 2) --scalling the screen for pixelart
-   len = #objects 
-    for i = 1, len, 1 do
-        objects[i]:draw() --drawing every object in order
+    --scalling the screen up for pixelart
+   love.graphics.scale(2, 2) 
+
+   --drawing every object in order
+   for _, obj in ipairs(objects) do
+        obj:draw() 
     end
 
-    love.graphics.scale(0.5, 0.5) --scaling for the UI
+    --scaling down for the UI
+    love.graphics.scale(0.5, 0.5)
     love.graphics.print('Use left and right arrows to change the level.\nWhite squares are entities.', 10, 10)
 end
